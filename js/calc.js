6a25c0280e9d47ae3b7e3895b96c6e294c247dd2
People34 <99544693+People34@users.noreply.github.com>
2023-07-25 17:01:43 +0300

    Add files via upload

--- js/calc.js
+++ js/calc.js
@@ -1,35 +1,34 @@
-// Функция, которая будет вызываться при клике на кнопку "Замовити"
 function updateOrderInfo() {
-    const quantityField = document.querySelector('.services__card-input.sum');
-    const quantity = parseInt(quantityField.value);
-    const price = parseFloat(document.querySelector('.services__card-button.active').dataset.price);
-    
-    // Проверка на заполненность поля с количеством
-    if (Number.isNaN(quantity) || quantity <= 0) {
-        const errmsgElement = document.getElementById('errmsg');
-        errmsgElement.innerHTML = 'Оберiть послугу';
-        errmsgElement.classList.remove('error');
-    } else {
-        const totalPrice = (price * quantity).toFixed(2); // Округляем до 2 знаков после запятой
-        const errmsgElement = document.getElementById('errmsg');
-        errmsgElement.innerHTML = `До сплати ${totalPrice} грн`;
-        
-        // Добавляем класс "error" если заказ меньше 70 грн
-        if (totalPrice < 70) {
-            errmsgElement.classList.add('error');
+    // Находим все карточки services__card
+    $('.services__card').each(function(index) {
+        const quantityField = $(this).find('.services__card-input.sum');
+        const quantity = parseInt(quantityField.val());
+        const price = parseFloat($(this).find('.services__card-button.active').data('price'));
+        const errmsgElement = $('#errmsg' + (index + 1));
+
+        // Проверка на заполненность поля с количеством
+        if (Number.isNaN(quantity) || quantity <= 0) {
+            errmsgElement.text('Оберіть кількість');
+            errmsgElement.removeClass('error');
         } else {
-            errmsgElement.classList.remove('error');
+            const totalPrice = (price * quantity).toFixed(2);
+            errmsgElement.text(`До сплати ${totalPrice} грн`);
+
+            // Добавляем класс "error" если заказ меньше 70 грн
+            if (totalPrice < 70) {
+                errmsgElement.addClass('error');
+            } else {
+                errmsgElement.removeClass('error');
+            }
         }
-    }
+    });
 }
 
 // Вызываем функцию для обновления информации о заказе при загрузке страницы
 updateOrderInfo();
 
 // Находим все кнопки "Замовити" и добавляем обработчик клика для каждой из них
-const orderButtons = document.querySelectorAll('.services__card-done');
-orderButtons.forEach(button => button.addEventListener('click', updateOrderInfo));
+$('.services__card-done').on('click', updateOrderInfo);
 
 // Добавляем обработчик изменения для поля с количеством
-const quantityField = document.querySelector('.services__card-input.sum');
-quantityField.addEventListener('input', updateOrderInfo);
+$('.services__card-input.sum').on('input', updateOrderInfo);
