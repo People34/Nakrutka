bd0b4d2b16470c2a8204fdb7403e28230c1f0032
People34 <99544693+People34@users.noreply.github.com>
2023-07-26 18:45:39 +0300

    Add files via upload

--- js/calc.js
+++ js/calc.js
@@ -1,34 +1,33 @@
 function updateOrderInfo() {
-    // Находим все карточки services__card
     $('.services__card').each(function(index) {
         const quantityField = $(this).find('.services__card-input.sum');
         const quantity = parseInt(quantityField.val());
         const price = parseFloat($(this).find('.services__card-button.active').data('price'));
         const errmsgElement = $('#errmsg' + (index + 1));
 
-        // Проверка на заполненность поля с количеством
         if (Number.isNaN(quantity) || quantity <= 0) {
-            errmsgElement.text('Оберіть кількість');
+            errmsgElement.text('Оберiть послугу');
             errmsgElement.removeClass('error');
         } else {
             const totalPrice = (price * quantity).toFixed(2);
-            errmsgElement.text(`До сплати ${totalPrice} грн`);
 
-            // Добавляем класс "error" если заказ меньше 70 грн
             if (totalPrice < 70) {
                 errmsgElement.addClass('error');
+                errmsgElement.text('Мiнiмальна вартiсть 70 грн');
             } else {
                 errmsgElement.removeClass('error');
+                errmsgElement.text(`До сплати ${totalPrice} грн`);
             }
         }
     });
 }
 
-// Вызываем функцию для обновления информации о заказе при загрузке страницы
 updateOrderInfo();
 
-// Находим все кнопки "Замовити" и добавляем обработчик клика для каждой из них
-$('.services__card-done').on('click', updateOrderInfo);
+$('.services__card-done').on('click', function() {
+    updateOrderInfo();
+});
 
-// Добавляем обработчик изменения для поля с количеством
-$('.services__card-input.sum').on('input', updateOrderInfo);
+$('.services__card-input.sum').on('input', function() {
+    updateOrderInfo();
+});
